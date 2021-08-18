a507ef6820f703178aeaab119accfbf3968b6595
di3po <bolatbekd1n@gmail.com>
2021-08-18 08:54:14 -0400

    initial commit 2

--- index.php
+++ index.php
@@ -404,7 +404,8 @@ $writerOfProduct->write($product1, $product2); */
     находиться только объявления, но не тела методов. Как и с абстрактным классом, в любом классе,
     поддерживающем этот интерфейс, должны реализоваться все определенные в нем методы. Иначе класс
     должен быть объявлен как абстрактный. */
-interface Chargeable {
+
+/* interface Chargeable {
     public function getPrice() : float;
 }
 
@@ -414,4 +415,321 @@ class ShopProduct implements Chargeable {
     public function getPrice() : float {
         return $this->price;
     }
+} */
+
+/*  Полезность интерфейсов - объединение типов данных, не связанных между собой. */
+
+// Extends - 1, Implements - 1 and more (by comma)
+//class Example extends Parent implements Interface1, Interface2 {}
+
+//  *** Трейты (типаж, особенность, характерная черта)
+//  *** в PHP нет множественного наследования(Multiple inheritance)
+/* trait PriceUtilities {
+    private $taxRate = 17;
+    public function calculateTax(float $price) : float {
+        return (($this->taxRate/100)*$price);
+    }
 }
+
+class ShopProduct {
+    use PriceUtilities;
+}
+
+abstract class Service {}
+
+class UtilityService extends Service {
+    use PriceUtilities;
+}
+
+$p = new ShopProduct();
+print $p->calculateTax(100)."</br>";
+
+$u = new UtilityService();
+print $u->calculateTax(100); */
+
+
+//  *** Обработка ошибок
+//  *** Исключение - объект класса Exception, предназначены для хранения информации об ошибках или
+//      выдачи сообщения о них. Конструктору класса Exception передаются 2 аргумента: строка сообщения
+//      и код ошибки. 
+
+//  \Exception
+
+/*
+    throw new \Exception -  этот оператор останавливает выполнение текущего кода и передает ответственность за
+                        обработку ошибок обратно в вызывающий код.
+*/
+
+/* public function __construct (string $file) {
+    $this->file = $file;
+    
+    if(!file_exists($file)) {
+        throw new \Exception(" '$file' doesn't exist.");
+    }
+
+    $this->xml = simplexml_load_file($file);
+} */
+
+/* try {
+
+} catch(\Exception $e) {
+    die($e->__toString());
+} */
+
+//  Создание подклассов класса Exception
+
+/* class XmlException extends \Exception {
+    private $error;
+
+    public function __construct(\LibXMLError $error) {
+        $shortfile = basename($error->file);
+        $msg = "[{$shortfile}, строка {$error->line}, стобец {$error->column}] {$error->message}";
+        $this->error = $error;
+        parent::__construct($msg, $error->code);  // сообщение и код ошибки
+    }
+
+    public function getLibXmlError() {
+        return $this->error;
+    }
+}
+
+class FileException extends \Exception {}
+
+class ConfException extends \Exception {}
+
+class Conf {
+    private $file;
+    private $xml;
+    private $lastmatch;
+
+    public function __construct(string $file) {
+        $this->file = $file;
+
+        if(!file_exists($file)) {                                   // new!
+            throw new \FileException(" '$file' doesn't exist.");    // new!
+        }                                                           // new!
+        
+        $this->xml = simplexml_load_file($file, null, LIBXML_NOERROR);  // new!
+
+        if(! is_object($this->xml)) {                                   // new!
+            throw new \XmlException(libxml_get_last_error());           // new!
+        }                                                               // new!
+
+        $matches = $this->xml->xpath("/conf");                          // new!
+        if(! count($matches)) {                                         // new!
+            throw new \ConfException("Root file not found: conf");      // new!
+        }                                                               // new!
+    }
+
+    public function write() {
+        if(! is_writable($this->file)) {                                    // new!
+            throw new \FileException(" '{$this->file}' isn't writable");    // new!
+        }                                                                   // new!
+
+        file_put_contents($this->file, $this->xml->asXML());
+    }
+
+    public function get(string $str) {
+        $matches = $this->xml->xpath("/conf/item[@name=\"$str\"]");
+        if(count($matches)) {
+            $this->lastmatch = $matches[0];
+            return (string)$matches[0];
+        }
+        return null;
+    }
+
+    public function set(string $key, string $value) {
+        if(! is_null($this->get($key))) {
+            $this->lastmatch = $value;
+            return;
+        }
+        $conf = $this->xml->conf;
+        $this->xml->addChild('item', $value)->addAttribute('name', $key);
+    }
+
+    public static function init() {
+        try{
+            $conf = new Conf(__DIR__."conf.broken.xml");
+            print "user: ".$conf->get('user')."\n";
+            print "password: ".$conf->get('password')."\n";
+            print "host: ".$conf->get('host')."\n";
+            $conf->set("password", "newpass");
+            $conf->write();
+        }
+        catch(FileException $e) {// File doesn't exist or not available }
+        catch(XmlException $e) {// Broken XML file }
+        catch(ConfException $e) {// Wrong XML file format }
+        catch(\Exception $e) {// Mustn't be invited }
+    }
+}
+
+/* <?xml version="1.0"?>
+<conf>
+    <item name="user">dino</item>
+    <item name="password">newpass</item>
+    <item name="host">localhost</item>
+</conf> */
+
+
+//  *** Очистка после операторов try/catch с помощью оператора finally
+
+ /*class Runner extends Conf { 
+    public static function init() {
+        try{
+            $fh = fopen(__DIR__."/log.txt", "a");
+            fputs($fh, "Start\n");
+            $conf = new Conf(dirname(__FILE__)."conf.broken.xml");
+            print "user: ".$conf->get('user')."\n";
+            print "host: ".$conf->get('host')."\n";
+            $conf->set("password", "newpass");
+            $conf->write();
+        }
+        catch(FileException $e) {
+            // File doesn't exist or not available 
+            fputs($fh, "Problem with file\n");
+            throw $e;
+        }
+        catch(XmlException $e) {
+            // Broken XML file 
+            fputs($fh, "Problem(s) with XML file\n");
+            throw $e;
+        }
+        catch(ConfException $e) {
+            // Wrong XML file format 
+            fputs($fh, "Problem(s) with configuration\n");
+            throw $e;
+        }
+        catch(\Exception $e) {
+            // Mustn't be invited 
+            fputs($fh, "Not excepted problem(s)\n");
+        }
+
+        //  блок оператора finally вып-ся только если в блоке оператора try или catch 
+        //  не вызываются функции die() или exit()
+        finally{
+            fputs($fh, "End\n");
+            fclose($fh);
+        }
+    }   
+ } */
+
+
+
+//  *** Завершенные методы и классы
+// final - конец наследованию
+
+/* class Checkout {
+    final public function totalize() {
+        // get sum of all
+    }
+} */
+
+
+//  *** Внутренний класс Error
+/* try {
+    eval("Wrong code");
+} catch(\Error $e) {
+    print get_class($e)."\n";
+} catch(\Exception $e) {
+    throw $e;
+} */
+// Result of compile: ParseError
+
+
+//  *** Методы-перехватчики
+//  Эти методы могут перехватывать сообщения, посланные несуществующим(неопределенным) методам или свойствам.
+//  В PHP есть 3 вида методов-перехватчиков:
+//  1 - Get:    __get($property)                        /*
+//  2 - Set:    __set($property, $value)                   для неопр.
+//              __isset($property)                         свойства
+//              __unset($property)                      */ 
+//  3 - Call    __call($method, $arg_array)             // для неопр. метода
+//              __callStatic($method, $arg_array)       // для неопр. метода
+
+/* class Person {
+    public function __get(string $property) {
+        $method = "get{$property}";             // MAGIC is here! get{Name}, get{Age}, so why $p->name works, 
+                                                // even if there's no $name property given
+        if(method_exists($this, $method)) {
+            return $this->$method();
+        }
+    }
+
+    public function getName() {
+        return "MyNameIsName";
+    }
+
+    public function getAge() {
+        return 99;
+    }
+}
+
+$p = new Person();
+print $p->name;     //  magic!
+print $p->weight;   //  won't work */
+
+
+/* class Student {
+    private $_name;
+    private $_age;
+
+    public function __set($property, $value)
+    {
+        $method = "set{$property}";
+        if(method_exists($this, $method)) {
+            return $this->$method($value);      // return method and value, or new "property" we will give later
+        }
+    }
+
+    public function setName($n) {
+        $this->_name = $n;
+        if(! is_null($n)) {
+            return $this->_name = mb_strtoupper($this->_name);
+        }
+    }
+
+    public function getName() {
+        return $this->_name;
+    }
+
+}
+
+$s = new Student();
+$s->name = "Bob Dylan";     //  $_name = "Bob Dylan" which will be written as "BOB DYLAN"
+print $s->getName(); */
+
+//  *** Метод __call() можно применять для целей делегирования. Значение, возвращаемое этим методом передается обратно
+//      клиенту так, будто оно было бы возвращено вызванным несуществующим методомю
+//  *** Делегирование - это механизм, с помощью которого один объект может вызвать метод другого объекта.
+class TaskWriter {
+    public function writeTask(Task $p) {
+        print $p->getTaskName();
+    }
+}
+
+class Task {
+    private $writer;
+
+    public function __construct(TaskWriter $writer) {
+        $this->writer = $writer;
+    }
+    
+    public function __call(string $method, array $arguments)
+    {
+        if(method_exists($this->writer, $method)){
+            return $this->writer->$method($this);
+        } else {
+            print "<br>{$method}() doesn't exist";
+        }
+    }
+
+    public function getTaskName() : string {
+        return "MyTaskName";
+    }
+}
+
+$t = new Task(new TaskWriter());
+$t->writeTask();            // will work thanks to __call() method, which invites writeTask() from TaskWriter class
+$t->writeAnotherThing();    // returns nothing, i expected some error messages or __call() method to invite this not existing
+                            //  writeAnotherThing() method and say that it doesn't exist. upd: Lol, I wrote it myself.
+
